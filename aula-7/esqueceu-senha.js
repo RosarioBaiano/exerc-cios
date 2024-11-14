99300d5ce0d9a630fcc7c89e25e4f6899422cf6c
Rosário Baiano <aleixobaiano@icloud.com>
2024-11-14 14:49:28 +0100

    adicionando arquivos dos exercícios

---
+++ aula-7/esqueceu-senha.js
@@ -0,0 +1,39 @@
+﻿window.onload = function (e) {
+
+    var btnEsqueceu = document.getElementById("btnEsqueceu");
+
+    var txtEmail = document.getElementById("txtEmail");
+
+    txtEmail.focus();
+
+    btnEsqueceu.onclick = function (e) {
+
+        e.preventDefault();
+
+        var email = txtEmail.value;
+
+        if (email == "") {
+            exibirMensagemErro("Email obrigatório.");
+
+        }
+        else {
+            realizarLogin(email);
+        }
+    };
+    function exibirMensagemErro(mensagem) {
+
+        var spnErro = document.getElementById("spnErro");
+
+        spnErro.innerText = mensagem;
+
+        spnErro.style.display = "block";
+
+        setTimeout(function () {
+            spnErro.style.display = "none";
+        }, 5000);
+    }
+    function realizarLogin(email) {
+
+        alert("O login foi realizado pelo usuário " + email);
+    }
+}
